732f92f54cac271929ab8910ee5f7b99523d3a84
Kular <kular.ai@gmail.com>
2014-08-19 01:06:12 +0900

    add clear.sh for removing vim backupfiles(*.*~)

---
+++ clear.sh
@@ -0,0 +1,22 @@
+results=(`find . -name "*~" -print`)
+length=${#results[@]}
+
+if [ "${length}" -eq 0 ]; then
+    echo "No files found."
+    exit 1
+else
+    for (( i=0; i<${length}; i++ ));
+    do
+        echo ${results[$i]}
+    done
+fi
+
+echo "Sure to remove files above ? (y/n)"
+read answer
+if [ "$answer" = "y" ]; then 
+    find . -name "*~" -print0 | xargs -0 rm
+    echo "Deleted!"
+else
+    echo "Cancelled."
+fi
+exit 0
